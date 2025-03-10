5aaab6cd2d8d0c9ab30341705098cfc22d7f0769
ipoellasiso <ipoellasiso86@gmail.com>
2025-03-10 20:57:53 +0800

    up login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
             'title'  => 'Halaman Login',
         );
 
-        return view('Auth.login', $data);
+        return view('Auth.Login', $data);
     }
 
     public function cek_login(Request $request)
